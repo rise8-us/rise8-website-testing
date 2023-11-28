4a6ec0b16e5e82a005ba49d145c71454728318dd
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-11-28 13:51:56 -0700

    parses json result of lighthouse scan

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,12 +24,12 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-const myCustomLHConfig: LH.Config.Json = {
-  extends: 'lighthouse:default',
-  settings: {
-    onlyCategories: ['pwa']
-  }
-};
+// const myCustomLHConfig: LH.Config.Json = {
+//   extends: 'lighthouse:default',
+//   settings: {
+//     onlyCategories: ['pwa']
+//   }
+// };
 
 // test('lighthouse performance test', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
@@ -41,8 +41,8 @@ const myCustomLHConfig: LH.Config.Json = {
 // });
 
 const sitePages = ['https://www.rise8.us',
-  'https://www.rise8.us/about',
-    'https://www.rise8.us/careers']
+  'https://www.rise8.us/about']
+    //'https://www.rise8.us/careers']
 
     // https://www.rise8.us/cato-report
     //
@@ -63,38 +63,41 @@ const sitePages = ['https://www.rise8.us',
     // https://www.rise8.us/resources/a-defense-modernization-story]
 
 
-test('another test', async ({ page }) => {
-  const chrome = await chromeLauncher.launch();
+test('lighthouse test', async ({ page }) => {
 
   const { myCustomLHConfig } = await import('./lh-config-ts');
 
-  // for (const pageName of sitePages) {
-  //   const results = await lighthouse(
-  //       pageName,
-  //       {
-  //         port: chrome.port,
-  //         output: 'html'
-  //       },
-  //       myCustomLHConfig
-  //   );
-  //   //var urlRegex = /(https?:\/\/[^\s]+)/;
-  //   const simplifiedUrl = pageName.replace('https://www.', '');
-  //   console.log(simplifiedUrl);
-  // //const fileName = simplifiedUrl.concat('lh-report.html');
-  //   //await writeFile(fileName, results.report);
-  //   chrome.kill();
-  // }
+  for (const curPage of sitePages) {
+    const chrome = await chromeLauncher.launch();
 
-  const results = await lighthouse(
-      'https://www.rise8.us',
-      {
-        port: chrome.port,
-        output: 'html'
-      },
-      myCustomLHConfig
-  );
+    const results = await lighthouse(
+        curPage,
+        {
+          port: chrome.port,
+          output: 'json'
+        },
+        myCustomLHConfig
+    );
+    //var urlRegex = /(https?:\/\/[^\s]+)/;
+  //   const simplifiedUrl = curPage.replace('https://www.', '').replace('/', '');
+  //
+  // const fileName = simplifiedUrl.concat('-lh-report.json');
+  //   await writeFile(fileName, results.report);
+    chrome.kill();
 
-  await writeFile('./lh-local_preset-report1.html', results.report);
-  await chrome.kill();
+    await writeFile('resultfile', results.lhr.categories.performance.score.toString());
+  }
+
+  // const results = await lighthouse(
+  //     'https://www.rise8.us/about',
+  //     {
+  //       port: chrome.port,
+  //       output: 'html'
+  //     },
+  //     myCustomLHConfig
+  // );
+  //
+  // await writeFile('./lh-local_preset-report2.html', results.report);
+  // chrome.kill();
 });
 
