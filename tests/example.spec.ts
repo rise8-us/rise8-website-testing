6d2bc442bbc60c25748c84fd0a76018f9cafe624
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-11-28 12:57:34 -0700

    working lighthouse test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
-//import lighthouse from "lighthouse";
+import lighthouse from 'lighthouse';
+//const lighthouse = require('lighthouse/core/index.cjs')
+import * as chromeLauncher from 'chrome-launcher';
+import { writeFile } from 'fs/promises';
 
 
 test('has title', async ({ page }) => {
@@ -21,31 +24,77 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('lighthouse performance test', async ({ page }) => {
+const myCustomLHConfig: LH.Config.Json = {
+  extends: 'lighthouse:default',
+  settings: {
+    onlyCategories: ['pwa']
+  }
+};
 
-  await lighthouse('https://playwright.dev/', {port: 9222});
-});
-//import config from './path/to/custom-config.js';
+// test('lighthouse performance test', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//
+//   await lighthouse('https://rise8.us');
+// });
 
-// const { chromium } = require('playwright');
-import lighthouse from 'lighthouse';
-test('lighthouse performance test2', async () => {
-  // Launch the browser
-  const browser = await chromium.launch();
-  // Create a new page
-  const page = await browser.newPage();
-  // Navigate to the website you want to test
-  await page.goto('https://example.com');
-  // Run Lighthouse audit
-  const { lhr } = await lighthouse(page.url(), {
-    port: new URL(browser.wsEndpoint()).port,
-    output: 'json',
-  });
-  // Access the performance score
-  const performanceScore = lhr.categories.performance.score;
-  // Output the performance score
-  console.log(`Performance Score: ${performanceScore * 100}`);
-  // Close the browser
-  await browser.close();
+const sitePages = ['https://www.rise8.us',
+  'https://www.rise8.us/about',
+    'https://www.rise8.us/careers']
+
+    // https://www.rise8.us/cato-report
+    //
+    // https://www.rise8.us/contact
+    //
+    // https://www.rise8.us/privacy-policy
+    //
+    // https://www.rise8.us/prodacity
+    //
+    // https://www.rise8.us/resource/blog
+    //
+    // https://www.rise8.us/solutions/solutions
+    //
+    // https://www.rise8.us/solutions/tracer
+    //
+    // https://www.rise8.us/event-rsvp
+    //
+    // https://www.rise8.us/resources/a-defense-modernization-story]
+
+
+test('another test', async ({ page }) => {
+  const chrome = await chromeLauncher.launch();
+
+  const { myCustomLHConfig } = await import('./lh-config-ts');
+
+  // for (const pageName of sitePages) {
+  //   const results = await lighthouse(
+  //       pageName,
+  //       {
+  //         port: chrome.port,
+  //         output: 'html'
+  //       },
+  //       myCustomLHConfig
+  //   );
+  //   //var urlRegex = /(https?:\/\/[^\s]+)/;
+  //   const simplifiedUrl = pageName.replace('https://www.', '');
+  //   console.log(simplifiedUrl);
+  // //const fileName = simplifiedUrl.concat('lh-report.html');
+  //   //await writeFile(fileName, results.report);
+  //   chrome.kill();
+  // }
+
+  const results = await lighthouse(
+      'https://www.rise8.us',
+      {
+        port: chrome.port,
+        output: 'html'
+      },
+      myCustomLHConfig
+  );
+
+  await writeFile('./lh-local_preset-report1.html', results.report);
+  await chrome.kill();
 });
 
