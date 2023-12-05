44c6879ea05cae8d2912c87326092940a43f0900
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-12-05 12:51:32 -0700

    adds slack webhook to secret

--- automation/automationScript.sh
+++ automation/automationScript.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+SLACK_SECRET="$1"
+if [ "$SLACK_SECRET" == null ]
+then
+    echo "Please provide a slack secret"
+    exit 1
+fi
+
 # Path to the JSON file
 JSON_FILE="./test-results/reports-test.json"
 
@@ -9,15 +16,14 @@ performance_score=$(jq -r '.categories.performance | .score * 100' "$JSON_FILE")
 # Threshold for the alert
 performance_threshold=90
 
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $SLACK_SECRET
 
 # Check if the performance score is below the threshold
 if (( $(echo "$performance_score < $performance_threshold" | bc -l) )); then
     # Generate an alert
     echo "Alert: Performance score is below 90. Current score: $performance_score"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Performance failed! score: $performance_score\"}" https://hooks.slack.com/services/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
-    #curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Performance failed!score: $performance_score\"}" https://hooks.slack.com/services/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else
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Performance failed! score: $performance_score\"}" $SLACK_SECRET
 else
     echo "Performance score is above or equal to 90. Current score: $performance_score"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"Performance passed!"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Performance passed!"}' ${SLACK_SECRET}
 fi
