cfd901068b0a69bb19eb6546fe4a65f172614cc8
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-12-05 16:48:12 -0700

    makes checkScore function

--- automation/automationScript.sh
+++ automation/automationScript.sh
@@ -7,6 +7,12 @@ threshold=90
 
 # Get the performance score using jq, convert to float, and multiply by 100
 performance_score=$(jq -r '.categories.performance | .score * 100' "$JSON_FILE")
+accessibility_score=$(jq -r '.categories.accessibility | .score * 100' "$JSON_FILE")
+best_practices_score=$(jq -r '.categories.best-practices | .score * 100' "$JSON_FILE")
+seo_score=$(jq -r '.categories.seo | .score * 100' "$JSON_FILE")
+pwa_score=$(jq -r '.categories.pwa | .score * 100' "$JSON_FILE")
+
+
 echo "Performance score: $performance_score"
 
 
@@ -19,3 +25,20 @@ else
     echo "Performance score is above or equal to 90. Current score: $performance_score"
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Performance score is above or equal to 90. Current score: $performance_score\"}" "${SLACK_SECRET}"
 fi
+
+
+function checkScore() {
+  # Check if the category score is below the threshold
+  if (( $(echo "$1 < $threshold" | bc -l) )); then
+      # Generate an alert
+      echo "Alert: $2 score is below 90. Current score: $1"
+      curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 score is below 90! $2 score: $1\"}" "$SLACK_SECRET"
+  else
+      echo "$2 score is above or equal to 90. Current score: $1"
+      curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 score is above or equal to 90. $2 score: $1\"}" "${SLACK_SECRET}"
+  fi
+}
+
+checkScore "$accessibility_score" "Accessibility"
+
+
