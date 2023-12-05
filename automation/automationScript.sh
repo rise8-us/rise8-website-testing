895dd34f4613d85a88172ab7cd95ee8061ed254d
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-12-05 14:56:29 -0700

    use artifact in script

--- automation/automationScript.sh
+++ automation/automationScript.sh
@@ -6,10 +6,10 @@
 #    echo "Please provide a slack secret"
 #    exit 1
 #fi
-pwd
-ls
+
 # Path to the JSON file
-JSON_FILE="./test-results/reports-test.json"
+#JSON_FILE="./test-results/reports-test.json"
+JSON_FILE=lighthouse-results
 
 # Get the performance score using jq, convert to float, and multiply by 100
 performance_score=$(jq -r '.categories.performance | .score * 100' "$JSON_FILE")
