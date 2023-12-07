38bb1de81a5db97b489b660d234c63f3b53456d4
Delaney Coveno <decoveno.ctr@kr.af.mil>
2023-12-06 17:44:09 -0700

    dynamically pulls all site pages, runs lighthouse scan on all pages, and checks score and sends message for all results

--- automation/automationScript.sh
+++ automation/automationScript.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-# Path to the JSON file
-JSON_FILE="reports-test.json"
-# Threshold for the alert
+#dynamically determine all site pages
+sitemap=$(curl https://www.rise8.us/sitemap.xml)
+parsed=$(echo "$sitemap" | sed "s/<.*>//g")
+
+LIGHTHOUSE_REPORTS_FOLDER="../lighthouse/"
 threshold=90
 
-# Get the performance score using jq, convert to float, and multiply by 100
-performance_score=$(jq -r '.categories.performance | .score * 100' "$JSON_FILE")
-accessibility_score=$(jq -r '.categories.accessibility | .score * 100' "$JSON_FILE")
-best_practices_score=$(jq -r '.categories | .["best-practices"] | .score * 100' "$JSON_FILE")
-seo_score=$(jq -r '.categories.seo | .score * 100' "$JSON_FILE")
-pwa_score=$(jq -r '.categories.pwa | .score * 100' "$JSON_FILE")
-
+#check the score against the threshold and send a slack message if it fails
 function checkScore() {
-  # Check if the category score is below the threshold, and send slack alert if it fails (TODO: sends message either way right now)
-  # TODO: send the slack message to the correct channel
+  # TODO: send the slack message to the correct channel - maybe lower threshold so the channel isn't spammed?
   if (( $(echo "$1 < $threshold" | bc -l) )); then
-      curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 failed. Score is below $threshold. $2 score: $1\"}" "$SLACK_SECRET"
-  else
-      curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 passed. Score is above or equal to $threshold. $2 score: $1\"}" "${SLACK_SECRET}"
+      curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 failed for page: $3. Score is below $threshold. $2 score: $1\"}" "$SLACK_SECRET"
+  #else
+      #curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$2 passed for page: $3. Score is above or equal to $threshold. $2 score: $1\"}" "${SLACK_SECRET}"
   fi
 }
 
-checkScore "$performance_score" "Performance"
-checkScore "$accessibility_score" "Accessibility"
-checkScore "$best_practices_score" "Best Practices"
-checkScore "$seo_score" "SEO"
-checkScore "$pwa_score" "PWA"
+while read -r line
+do
+  if [[ ! -z "${line//[[:space:]]*}" ]]; then
+    echo "line:" "$line"
+    nameForMessage=$(echo "$line" | tr -d '[:space:]');
+    filename=$(echo "$line" | sed "s/https:\/\/www\.//g" | sed "s/\./-/g" | sed "s/\//-/g" | tr -d '[:space:]');# | sed "s/$/delaney/")
+    REPORT_FILE="$LIGHTHOUSE_REPORTS_FOLDER""$filename"".json"
+    echo "REPORT_FILE: $REPORT_FILE"
+
+    # Get the performance score using jq, convert to float, and multiply by 100
+    performance_score=$(jq -r '.categories.performance | .score * 100' "$REPORT_FILE")
+    accessibility_score=$(jq -r '.categories.accessibility | .score * 100' "$REPORT_FILE")
+    best_practices_score=$(jq -r '.categories | .["best-practices"] | .score * 100' "$REPORT_FILE")
+    seo_score=$(jq -r '.categories.seo | .score * 100' "$REPORT_FILE")
+    pwa_score=$(jq -r '.categories.pwa | .score * 100' "$REPORT_FILE")
+
+    checkScore "$performance_score" "Performance" "$nameForMessage"
+    checkScore "$accessibility_score" "Accessibility" "$nameForMessage"
+    checkScore "$best_practices_score" "Best Practices" "$nameForMessage"
+    checkScore "$seo_score" "SEO" "$nameForMessage"
+    checkScore "$pwa_score" "PWA" "$nameForMessage"
+  fi
+done <<< "$parsed"
+
 
 
--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -2,38 +2,56 @@ import { playAudit } from 'playwright-lighthouse';
 import * as playwright from 'playwright';
 import { test } from '@playwright/test';
 import { describe } from 'node:test';
-
+import axios from 'axios';
+import xmljs from 'xml-js';
 
 describe('Lighthouse Automation Test', () => {
 
     test('Performance Test ', async ({ browserName }) => {
+        let  data: { urlset: { url: { loc: { _text: any; }; }[]; }; };
+        try {
+            const response = await axios.get("https://www.rise8.us/sitemap.xml").then((response) => {
+                data = JSON.parse(
+                    xmljs.xml2json(response.data, { compact: true, spaces: 2 })
+                );
+            });
+        } catch (error) {
+            throw new Error("Failed to fetch website urls. Exiting.");
+        }
+
+        test.setTimeout(3000000);
         test.skip(browserName !== 'chromium', 'Still working on it');
-        const browser = await playwright['chromium'].launch({
-            args: ['--remote-debugging-port=9222'],
-        });
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        await page.goto('https://www.rise8.us/');
 
-        await playAudit({
-            page: page,
-            thresholds: {
-                performance: 90,
-                accessibility: 90,
-                'best-practices': 90,
-                seo: 90,
-                pwa: 90,
-            },
-            ignoreError:true,
-            port: 9222,
-            reports: {
-                formats: {json: true},
-                name: 'reports-test',
-                directory: `${process.cwd()}/test-results`,
-            },
-        });
+        for (let i = 0; i < data.urlset.url.length; i++) {
+            const browser = await playwright['chromium'].launch({
+                args: ['--remote-debugging-port=9222'],
+            });
+            const context = await browser.newContext();
+            const page = await context.newPage();
+            const url = data.urlset.url[i].loc._text
+            await page.goto(url);
+            let fileName=url.replace('https:\/\/www\.', '').replace('\.', '-').replace('/\s/g', "").trim().split('/').join('-');
+            console.log(fileName);
 
-        await browser.close();
+            await playAudit({
+                page: page,
+                thresholds: {
+                    performance: 90,
+                    accessibility: 90,
+                    'best-practices': 90,
+                    seo: 90,
+                    pwa: 90,
+                },
+                ignoreError: true,
+                port: 9222,
+                reports: {
+                    formats: {json: true},
+                    name: fileName,
+                    directory: `${process.cwd()}/lighthouse-reports`,
+                },
+            });
+            await browser.close();
+        }
     });
 
 });
